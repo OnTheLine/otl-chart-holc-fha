6327ce16cdcb7adbb9f111a1d5fdd616f70b9034
Jack Dougherty <jack.dougherty@trincoll.edu>
2020-05-07 20:22:47 -0400

    upload all

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # otl-chart-holc-fha
- Chartjs line chart on HOLC and FHA funds 1933-48 for On The Line book
+
+Chartjs line chart on HOLC and FHA funds 1933-48 for On The Line book http://onthline.trincoll.edu
+
+Developed by Ilya Ilyankou and Jack Dougherty
+
+## View online
+
+https://ontheline.github.io/otl-chart-holc-fha/
+
+## Sources
+
+C. Lowell Harriss, History and Policies of the Home Owners’ Loan Corporation (New York: National Bureau of Economic Research, 1951), https://www.nber.org/books/harr51-1, Table 4: Loans Closed by HOLC, 1933-1936, p. 30.
+
+Federal Housing Administration, Fifteenth Annual Report of the Federal Housing Administration, 1949, https://www.huduser.gov/portal/publications/Fifteenth-Annual-Report-of-the-Federal-Housing-Administration.html, Table 1: Yearly Volume of Mortgages and Loans Insured by FHA Under All Titles: Face amount of mortgages written and net proceeds of loans insured, 1934-1948, p. 8.
+
+Note that chart compares HOLC mortgages versus all types of FHA-insured loans and mortgages. The majority of FHA is home mortgage insurance, but the total also includes property improvements and rental housing, which can be disaggregated in the source table.
+
+Also, note that the HOLC data above differs from US Federal Home Loan Bank Board, Annual Report: Federal Home Loan Bank Board (Washington, D.C.: Government Printing Office, 1941), https://catalog.hathitrust.org/Record/008441059, Chart XIX: Home Mortgage Lending Activity (including HOLC), p. 40.
---
+++ script.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+
+  var TITLE = 'Value of HOLC Mortgages and FHA-Insured Loans and Mortgages, 1933-48';
+
+  // Which column names contain data points?
+  var X_LABELS = ['1933','1934','1935','1936','1937','1938','1939','1940','1941','1942','1943','1944','1945','1946','1947','1948'];
+
+  // Which column name contains names (labels) for each series?
+  var NAMES = 'names';
+
+  // Optionally, which column contains color names for the series?
+  // If not specified, will apply default color scheme
+  var COLORS = 'color';
+
+  var X_AXIS = 'Year';  // x-axis label and label in tooltip
+  var Y_AXIS = 'Millions of Dollars'; // y-axis label and label in tooltip
+
+  var SHOW_GRID = true; // `true` to show the grid, `false` to hide
+  var SHOW_LEGEND = true; // `true` to show the legend, `false` to hide
+
+  // Read data file and create a chart
+  d3.csv('data.csv').then(function(rows) {
+
+    var datasets = rows.map(function(row) {
+
+      var dataset = {
+        label: row[NAMES],
+        fill: false,
+        data: X_LABELS.map(function(t) { return row[t] })
+      }
+
+      if (row[COLORS]) {
+        dataset.backgroundColor = row[COLORS];
+        dataset.borderColor = row[COLORS];
+        dataset.pointBackgroundColor = row[COLORS];
+        dataset.pointBorderColor = row[COLORS];
+      }
+
+      return dataset;
+
+    });
+
+		var lineChartData = {
+      labels: X_LABELS,
+			datasets: datasets
+    };
+
+    var ctx = document.getElementById('container').getContext('2d');
+
+    new Chart(ctx, {
+      type: 'line',
+      data: lineChartData,
+
+      options: {
+        title: {
+          display: true,
+          text: TITLE,
+          fontSize: 14,
+        },
+        legend: {
+          display: SHOW_LEGEND,
+        },
+        scales: {
+          xAxes: [{
+            scaleLabel: {
+              display: X_AXIS !== '',
+              labelString: X_AXIS
+            },
+            gridLines: {
+              display: SHOW_GRID,
+            },
+            ticks: {
+              callback: function(value, index, values) {
+                return value.toLocaleString();
+              }
+            }
+          }],
+          yAxes: [{
+            beginAtZero: true,
+            scaleLabel: {
+              display: Y_AXIS !== '',
+              labelString: Y_AXIS
+            },
+            gridLines: {
+              display: SHOW_GRID,
+            },
+            ticks: {
+              callback: function(value, index, values) {
+                return value.toLocaleString()
+              }
+            }
+          }]
+        },
+        tooltips: {
+          displayColors: false,
+          callbacks: {
+            label: function(tooltipItem, all) {
+              return all.datasets[tooltipItem.datasetIndex].label
+                + ': ' + tooltipItem.yLabel.toLocaleString();
+            }
+          }
+        },
+        plugins: {
+          colorschemes: {
+            scheme: 'brewer.Paired12'
+          }
+        }
+      }
+    });
+
+  });
+
+});
